b10e33d8abeaa85752b7631f2ca72d463649e186
pubudurathnathilaka14-wq <pubudurathnathilaka14@gmail.com>
2026-01-01 23:29:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://ibb.co/Tx8KNfTp",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DAKA-MD Is Alive Now💋*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
 
